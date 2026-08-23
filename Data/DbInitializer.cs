81f674d1e70a297ae2aaee0c63310b5a2fe9b6fc
MeganAe <ntkhang969@gmail.com>
2026-08-23 18:42:28 +0300

    ttt

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -5,7 +5,6 @@ public static class DbInitializer
     public static void Initialize()
     {
         using var context = new AppDbContext();
-        // Crée uniquement la structure : aucune donnée fictive n'est ajoutée.
         context.Database.EnsureCreated();
     }
 }
